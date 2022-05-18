1aebe15ae0f012344c27fa652c96e96363bf42d9
ldes-cou <ldes-cou@student.fr>
2022-05-18 12:22:53 +0200

    ok chan

--- Commands.cpp
+++ Commands.cpp
@@ -55,10 +55,10 @@ void join(Server &s, User &u, std::vector<std::string> arg) // exit ou quit
         s.chans[chan_name].add_user(chan_name, u);    
     }
     std::map<std::string, Channel>::const_iterator titi;
-    for (titi = s.chans.begin(); titi != s.chans.end(); titi++)
-    {
-        std::cout << titi->first << std::endl;
-        for (std::map<std::string, User>::const_iterator it = s.chans[titi->second].get_users()
-    }
+    // for (titi = s.chans.begin(); titi != s.chans.end(); titi++)
+    // {
+    //     std::cout << titi->first << std::endl;
+    //     for (std::map<std::string, User>::const_iterator it = s.chans[titi->second].get_users()
+    // }
 
 }
