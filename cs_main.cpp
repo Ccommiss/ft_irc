2895ba1b770d2759bdc843d757f6bc485c531365
Clement SEJAULT <csejault@e3r11p21.clusters.42paris.fr>
2022-05-18 17:10:44 +0200

    feat: create server_tcp / cs_main and cs_utils

---
+++ cs_main.cpp
@@ -0,0 +1,23 @@
+#include "cs_utils.hpp"
+#include "Server_tcp.hpp"
+#include <iostream>
+
+int main (int ac, char**av)
+{
+	if (ac != 3)
+	{
+		std::cerr << "Syntax needed : " << av[0] << " <port> <password>" << std::endl;
+		return(-1);
+	}
+	Server_tcp *sv;
+
+	try 
+	{
+		sv = new Server_tcp(av[1], av[2]);
+	}
+	catch (std::exception &	e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+}
